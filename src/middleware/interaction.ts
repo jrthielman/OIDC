8ab955817ef959ecd33f784d8c27c393f0ea1d57
jrthielman <javiel_55@hotmail.com>
2019-07-05 18:18:58 +0200

    Wat extra validatie toegevoegd. Het doet nog niks bijzonders

--- src/middleware/interaction.ts
+++ src/middleware/interaction.ts
@@ -7,8 +7,17 @@ export default async (req: express.Request, res: express.Response, next: express
     const provider = res.locals.provider;
     const details = await provider.interactionDetails(req);
 
+    if(details.interaction.error === 'login_required'){
+        await res.redirect(`/interaction/${details.uuid}/login`);
+        return next();
+    }
+
     if (req.cookies._session) {
-        await provider.interactionFinished(req, res, req.cookies['result']);
+        if(req.cookies['result'].login.acr === 'rookie'){
+            await provider.interactionFinished(req, res, req.cookies['result']);
+            return next();
+        }
+        await res.redirect(`/interaction/${details.uuid}/login`);
         return next();
     }
 
--- src/middleware/twoFactorInteraction.ts
+++ src/middleware/twoFactorInteraction.ts
@@ -9,7 +9,8 @@ export default async (req: express.Request, res: express.Response, next: express
     const {twoFactor} = req.body;
 
     if (twoFactor) {
-        return await provider.interactionFinished(req, res, req.cookies['result']);
+        return await provider.interactionFinished(req, res, req.cookies['result'])
     }
+
     return await res.redirect(`/interaction/${details.uuid}/twofactor#1`);
 }
